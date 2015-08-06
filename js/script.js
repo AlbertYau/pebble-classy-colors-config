d029702b733463ca0f95e756ae2f7300f1ccd9b2
Albert Yau <albert.yau@candyspace.com>
2015-08-06 16:32:16 -0400

    Test sending JSON config

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+var config = {
+	'bgColor': ''
+};
+
 $(document).ready(function() {
 
 	console.log("Document Ready");
 
 	$('#send').on('click', function() {
 		// Close config page and return data
-		location.href = 'pebblejs://close#success';
+		
+		console.log($('#bgColorPicker')[0].value);
+		config.bgColor = $('#bgColorPicker')[0].value;
+		location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(config));
+
 	});
 
 });
