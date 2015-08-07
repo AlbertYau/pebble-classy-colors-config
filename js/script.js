256094d37914f43e1c2620bf2f94dd8f17f284fa
Albert Yau <albert.yau@candyspace.com>
2015-08-07 11:49:07 -0400

    Save bgcolor var locally

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 var config = {
-	'bgColor': ''
+	'bgColor': 0
 };
 
 $(document).ready(function() {
 
 	console.log("Document Ready");
+	
+	// Init
+	var bgColorPicker = $('#bgColorPicker')[0];
+
+	// Check local storage
+    if(localStorage['bgColor']) {
+      bgColorPicker.value = localStorage['bgColor'];
+    }
 
 	$('#send').on('click', function() {
-		// Close config page and return data
-		
-		console.log($('#bgColorPicker')[0].value);
-		config.bgColor = $('#bgColorPicker')[0].value;
-		location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(config));
+		// Set options
+		config.bgColor = bgColorPicker.value;
 
+		// Store values
+		localStorage['bgColor'] = options['bgColor'];
+
+		// Close config page and return data
+		location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(config));
 	});
 
 });
