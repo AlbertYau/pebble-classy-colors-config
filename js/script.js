819c262ae114b8cbf1dc538b948c1c3f987f3fc3
Albert Yau <albert.yau@candyspace.com>
2015-08-14 16:36:14 -0400

    Add secondary color picker

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var config = {
 	'bgColor': '',
 	'timeColor': '',
+	'secondaryColor': '',
 	'bluetoothIconEnabled': 1
 };
 
@@ -11,6 +12,7 @@ $(document).ready(function() {
 	var bgColorPicker = $('#bgColorPicker');
 	var timeColorPicker = $('#timeColorPicker');
 	var bluetoothEnabledCheckBox = $('#bluetoothEnabledCheckBox');
+	var secondaryColorPicker = $('#secondaryColorPicker');
 
 	// check local storage
     if(localStorage.length > 0) {
@@ -30,6 +32,12 @@ $(document).ready(function() {
 	      colorSpan = timeColorPicker.siblings().find('.value');
 	      colorSpan.css('background-color', storedColor.replace(/^0x/, '#')); 	
       }
+      if (localStorage.getItem('secondaryColor') !== null) {
+	      storedColor = localStorage.getItem('secondaryColor');
+	      secondaryColorPicker.attr('value', storedColor);
+	      colorSpan = secondaryColorPicker.siblings().find('.value');
+	      colorSpan.css('background-color', storedColor.replace(/^0x/, '#')); 	
+      }      
       if (localStorage.getItem('bluetoothIconEnabled') !== null) {
 		  var bluetoothIconEnabled = localStorage.getItem('bluetoothIconEnabled') != 0 ? true : false;
 	  	  if (bluetoothIconEnabled) {
@@ -46,11 +54,13 @@ $(document).ready(function() {
 		// Set options
 		config.bgColor = bgColorPicker[0].value;
 		config.timeColor = timeColorPicker[0].value;
+		config.secondaryColor = secondaryColorPicker[0].value;
 		config.bluetoothIconEnabled = bluetoothEnabledCheckBox[0].checked ? 1 : 0;
 
 		// Store values
 		localStorage.setItem('bgColor', config.bgColor);
 		localStorage.setItem('timeColor', config.timeColor);
+		localStorage.setItem('secondaryColor', config.secondaryColor);
 		localStorage.setItem('bluetoothIconEnabled', config.bluetoothIconEnabled);
 
 		console.log(config);
